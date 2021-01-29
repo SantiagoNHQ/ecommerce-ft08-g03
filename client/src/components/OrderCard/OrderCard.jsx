a2bbade30be5d0e4f7c8bcbfc33c82a53463dfbb
GonzaG3X <rodriguezgonzalo97@gmail.com>
2021-01-29 20:51:03 -0300

    OrdersTable y OrderCard funcionales, ahora el admin puede ver las ordenes de todos los usuarios

---
+++ client/src/components/OrderCard/OrderCard.jsx
@@ -0,0 +1,8 @@
+export default function OrderCard({data}) {
+
+    return (
+        <div>
+            <p>userId: {data.userId} estado: {data.estado}</p>
+        </div>
+    )
+}
--- client/src/components/OrdersTable/OrdersTable.jsx
+++ client/src/components/OrdersTable/OrdersTable.jsx
@@ -2,6 +2,7 @@ import {useEffect} from 'react'
 import axios from 'axios'
 import { connect } from 'react-redux'
 import { ordersLoad } from "../../redux/actions";
+import OrderCard from '../OrderCard/OrderCard'
 
 const mapStateToProps = (state) => {
     return {
@@ -20,15 +21,17 @@ const mapDispatchToProps = (dispatch) => {
 function OrdersTable(props) {
     var {orders, onOrdersLoad} = props
     var estado = props.match.params.status
+    console.log("Status: ", estado)
+    
     useEffect(() => {
-        axios.get("http://localhost:3001/user/orders" + !estado ? "" : `/${estado}`).then(r => {
+        axios.get("http://localhost:3001/user/orders" + (!estado ? "" : `/${estado}`)).then(r => {
             onOrdersLoad(r.data)
         }).catch(e => console.log("Error: ", e))
     }, [estado, onOrdersLoad])
 
     return (
         <div>
-            {orders/*  && orders.map(v => <OrderCard data={v} />) */}
+            {orders && orders.map(v => <OrderCard data={v} />)}
         </div>
     )
 }
