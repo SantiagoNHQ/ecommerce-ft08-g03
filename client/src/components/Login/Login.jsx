23ef6e77a3d893fbdd781bc632d07008a531cdb8
GonzaG3X <rodriguezgonzalo97@gmail.com>
2021-02-05 17:17:10 -0300

    login andando...

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,40 +1,42 @@
 import React, { useState } from "react";
 import axios from 'axios';
 import "./Login.css";
-
 //  S61 : Crear Componente Login
 
 
 export default function NuevoUsuario(props) {
 
-    const [state, setState] = useState({/* nombre:"", descripcion:"" */})
+    const [state, setState] = useState({})
 
     function submit(e) {
         console.log("Usuario y contraseña: ", state)
         e.preventDefault()
-        axios.post("http://localhost:3001/auth/login",{ data: state})
-        // axios({
-        //     method: 'post', 
-        //     url: 'http://localhost:3001/auth/login',
-        //     data: {
-        //       state
-        //     }
-        //   })
+        axios.post("http://localhost:3001/auth/login", state)
         .then(res => {
-            console.log("RESPONDI", res)
-            alert("USER LOGUEADO")
-            // props.history.push("/");
-            // window.location.reload(true);
+            var user = res.data
+            var value = JSON.stringify(user);       //nos pasa el objeto a JSON
+            localStorage.setItem('user', value);        //guardamos ese JSON en el store
+            
+            props.history.push("/");
+            window.location.reload(true);
+
+            // const string = localStorage.getItem('user');
+            // value = JSON.parse(string);
+            // console.log("mi store es: ", value)
         })
         .catch (err => {
             console.log("Mallllllllllllllllllll", err)
+            alert("No logueado")
+            props.history.push("/user/ingresar");
+            window.location.reload(true);
         })
     }
 
    function cambios (e){
+       console.log("ESTADO", state)
         e.preventDefault()
         setState({...state, [e.target.name]: e.target.value})
-        }
+    }
     
     function submitEnter (e) {
         if (e.key === 'Enter'){
@@ -45,13 +47,12 @@ export default function NuevoUsuario(props) {
     return (
         <div className = 'formulario'>
            
-            <form className='iniciarSesion' /*action="http://localhost:3001/auth/login" method='POST' */>
+            <form onSubmit={submit} className='iniciarSesion' >
                 <h1>Iniciar Sesion</h1>
-                <input key="username" type="text" placeholder="username" onKeyPress={ submitEnter } onChange={cambios} name="username" />
-                <input key="password" type="password" placeholder="password" onKeyPress={ submitEnter } onChange={cambios} name="password" />
-                <input type="submit" key="boton" onSubmit={submit} value="Enviar" />
+                <input key="username" type="text" placeholder="username" onChange={cambios} name="username" />
+                <input key="password" onKeyPress={submitEnter} type="password" placeholder="password" onChange={cambios} name="password" />
+                <input type="submit" key="boton" value="Enviar" />
             </form>
         </div>
     )
 }
-
