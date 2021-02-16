f8f15d770bb354779a0d878bac02e935afdb1ef2
grgrioprogrammer <benjamin-g-2016@hotmail.com>
2021-02-16 15:04:09 -0300

    Cambios en el link de carrito, usuarios, botones, etc

--- api/src/app.js
+++ api/src/app.js
@@ -33,12 +33,12 @@ server.use(
 );
 
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3002"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
+  );
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
   next();
 });
